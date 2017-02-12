93c0e4d737d25b38d4403858934a0f0ff7e321fe
philrice <phil.rice@itv.com>
2017-02-12 19:24:34 +0000

    Tidying up Sample3 some more

--- modules/sample/src/main/scala/org/validoc/Sample3.scala
+++ modules/sample/src/main/scala/org/validoc/Sample3.scala
@@ -11,48 +11,7 @@ import scala.concurrent.duration._
 import scala.reflect.ClassTag
 
 
-//trait BruceSetup {
-//
-//  def mostPopularHttp[T, HttpReq, HttpRes](implicit s: IHttpSetup[T, HttpReq, HttpRes]): ServiceTag[T, HttpReq, HttpRes] = s.rawService("mostPopular")
-//
-//  def promotionHttp[T, HttpReq, HttpRes](implicit s: IHttpSetup[T, HttpReq, HttpRes]): ServiceTag[T, HttpReq, HttpRes] = s.rawService("promotion")
-//
-//  def programmeAndProductionsHttp[T, HttpReq, HttpRes](implicit s: IHttpSetup[T, HttpReq, HttpRes]): ServiceTag[T, HttpReq, HttpRes] = s.rawService("programmeAndProductions")
-//
-//  def getObject[T, HttpReq, HttpRes: ToServiceResponse, Req: ClassTag : ToServiceRequest : CachableKey, Res: ParserFinder : ClassTag : CachableResult]
-//  (timeToStale: Duration, timeToDead: Duration, maxSize: Int, rawService: ServiceTag[T, HttpReq, HttpRes])
-//  (implicit s: IHttpSetup[T, HttpReq, HttpRes], toHttpReq: (ServiceRequest) => HttpReq, nanoTimeService: NanoTimeService): ServiceTag[T, Req, Res] = {
-//    import s._
-//    (cached[Req, Res](timeToStale, timeToDead, maxSize) _ compose profiled[Req, Res] _ compose httpCallout) (rawService)
-//  }
-//
-//  def enrichedMostPopularService[T, HttpReq, HttpRes: ToServiceResponse](implicit s: IHttpSetup[T, HttpReq, HttpRes], toHttpReq: (ServiceRequest) => HttpReq, nanoTimeService: NanoTimeService): ServiceTag[T, MostPopularQuery, EnrichedMostPopular] =
-//    s.enrich[MostPopularQuery, MostPopular, EnrichedMostPopular, ProgrammeId, Programme](
-//      getObject[T, HttpReq, HttpRes, MostPopularQuery, MostPopular](2 minutes, 10 hours, 20, mostPopularHttp),
-//      getObject[T, HttpReq, HttpRes, ProgrammeId, Programme](2 minutes, 10 hours, 2000, programmeAndProductionsHttp), //
-//      { mp: MostPopular => mp.programmeIds }, //
-//      { (mp: MostPopular, programmes: Seq[Programme]) => EnrichedMostPopular(mp, programmes) }
-//    )
-//
-//  def enrichedPromotionService[T, HttpReq, HttpRes: ToServiceResponse](implicit s: IHttpSetup[T, HttpReq, HttpRes], toHttpReq: (ServiceRequest) => HttpReq, nanoTimeService: NanoTimeService) =
-//    s.enrich[HomePageQuery, Promotion, EnrichedPromotion, ProductionId, Production](
-//      getObject[T, HttpReq, HttpRes, HomePageQuery, Promotion](2 minutes, 10 hours, 20, promotionHttp),
-//      getObject[T, HttpReq, HttpRes, ProductionId, Production](2 minutes, 10 hours, 2000, programmeAndProductionsHttp), //
-//      { p: Promotion => p.productionIds }, //
-//      { (p: Promotion, productions: Seq[Production]) => EnrichedPromotion(p.name, productions) }
-//    )
-//
-//  def homePageService[T, HttpReq, HttpRes: ToServiceResponse](implicit s: IHttpSetup[T, HttpReq, HttpRes], toHttpReq: (ServiceRequest) => HttpReq, nanoTimeService: NanoTimeService) =
-//    s.merge[HomePageQuery, HomePage, MostPopularQuery, EnrichedMostPopular, HomePageQuery, EnrichedPromotion](
-//      enrichedMostPopularService,
-//      enrichedPromotionService,
-//      x => MostPopularQuery,
-//      x => x,
-//      HomePage.apply
-//    )
-//}
-
-class BruceSetup2[ HttpReq, HttpRes: ToServiceResponse](implicit  toHttpReq: (ServiceRequest) => HttpReq, nanoTimeService: NanoTimeService) {
+class PromotionSetup[HttpReq, HttpRes: ToServiceResponse](implicit toHttpReq: (ServiceRequest) => HttpReq, nanoTimeService: NanoTimeService) {
 
   def mostPopularHttp[T](implicit s: IHttpSetup[T, HttpReq, HttpRes]): ServiceTag[T, HttpReq, HttpRes] = s.rawService("mostPopular")
 
@@ -60,39 +19,33 @@ class BruceSetup2[ HttpReq, HttpRes: ToServiceResponse](implicit  toHttpReq: (Se
 
   def programmeAndProductionsHttp[T](implicit s: IHttpSetup[T, HttpReq, HttpRes]): ServiceTag[T, HttpReq, HttpRes] = s.rawService("programmeAndProductions")
 
-  def getObject[T, Req: ClassTag : ToServiceRequest : CachableKey, Res: ParserFinder : ClassTag : CachableResult]
-  (timeToStale: Duration, timeToDead: Duration, maxSize: Int, rawService: ServiceTag[T, HttpReq, HttpRes])(implicit s: IHttpSetup[T, HttpReq, HttpRes]): ServiceTag[T, Req, Res] =
-    s.cached[Req, Res](timeToStale, timeToDead, maxSize)(s.profiled(s.httpCallout(rawService)))
+  def enrichedMostPopularService[T](implicit s: IHttpSetup[T, HttpReq, HttpRes]): ServiceTag[T, MostPopularQuery, EnrichedMostPopular] = {
+    import s._
+    aggregate(
+      getCachedProfiledObject[MostPopularQuery, MostPopular](2 minutes, 10 hours, 20, mostPopularHttp),
+      getCachedProfiledObject[ProgrammeId, Programme](2 minutes, 10 hours, 2000, programmeAndProductionsHttp)).
+      enrich[EnrichedMostPopular]
+  }
 
+  def enrichedPromotionService[T](implicit s: IHttpSetup[T, HttpReq, HttpRes]) = {
+    import s._
+    aggregate(
+      getCachedProfiledObject[HomePageQuery, Promotion](2 minutes, 10 hours, 20, promotionHttp),
+      getCachedProfiledObject[ProductionId, Production](2 minutes, 10 hours, 2000, programmeAndProductionsHttp)).
+      enrich[EnrichedPromotion]
+  }
 
-  def enrichedMostPopularService[T](implicit s: IHttpSetup[T, HttpReq, HttpRes]): ServiceTag[T, MostPopularQuery, EnrichedMostPopular] =
-    s.enrich[MostPopularQuery, MostPopular, EnrichedMostPopular, ProgrammeId, Programme](
-      getObject[T, MostPopularQuery, MostPopular](2 minutes, 10 hours, 20, mostPopularHttp),
-      getObject[T, ProgrammeId, Programme](2 minutes, 10 hours, 2000, programmeAndProductionsHttp), //
-      { mp: MostPopular => mp.programmeIds }, //
-      { (mp: MostPopular, programmes: Seq[Programme]) => EnrichedMostPopular(mp, programmes) }
-    )
-
-  def enrichedPromotionService[T](implicit s: IHttpSetup[T, HttpReq, HttpRes]) =
-    s.enrich[HomePageQuery, Promotion, EnrichedPromotion, ProductionId, Production](
-      getObject[T, HomePageQuery, Promotion](2 minutes, 10 hours, 20, promotionHttp),
-      getObject[T, ProductionId, Production](2 minutes, 10 hours, 2000, programmeAndProductionsHttp), //
-      { p: Promotion => p.productionIds }, //
-      { (p: Promotion, productions: Seq[Production]) => EnrichedPromotion(p.name, productions) }
-    )
-
-  def homePageService[T](implicit s: IHttpSetup[T, HttpReq, HttpRes]) =
-    s.merge[HomePageQuery, HomePage, MostPopularQuery, EnrichedMostPopular, HomePageQuery, EnrichedPromotion](
+  def homePageService[T](implicit s: IHttpSetup[T, HttpReq, HttpRes]) = {
+    import s._
+    aggregate(
       enrichedMostPopularService,
-      enrichedPromotionService,
-      x => MostPopularQuery,
-      x => x,
-      HomePage.apply
-    )
+      enrichedPromotionService).
+      merge[HomePageQuery, HomePage](HomePage.apply)
+  }
 }
 
 
-object Sample3 extends  App {
+object Sample3 extends App {
 
   implicit object ServiceResponseForString extends ToServiceResponse[String] {
     override def apply(v1: String): ServiceResponse = ServiceResponse(Status.Ok, Body(v1), ContentType("text/plain"))
@@ -110,8 +63,10 @@ object Sample3 extends  App {
 
   implicit val nanoTimeService = SystemClockNanoTimeService
 
-  val bruce = new BruceSetup2[ String, String]()
-  import bruce._
+  val setup = new PromotionSetup[String, String]()
+
+  import setup._
+
   println(homePageService[String])
   println
   println(enrichedPromotionService[String])
--- modules/utils/src/main/scala/org/validoc/utils/service/ServiceLanguage.scala
+++ modules/utils/src/main/scala/org/validoc/utils/service/ServiceLanguage.scala
@@ -1,5 +1,7 @@
 package org.validoc.utils.service
 
+import org.validoc.utils.Service
+import org.validoc.utils.aggregate.{EnrichParentChildService, Enricher, HasChildren, MergeService}
 import org.validoc.utils.caching.{CachableKey, CachableResult}
 import org.validoc.utils.http.{ServiceRequest, ToServiceRequest, ToServiceResponse}
 import org.validoc.utils.parser.ParserFinder
@@ -12,7 +14,7 @@ case class ServiceTag[T, Req, Res](t: T)
 
 
 trait IService[T] {
-  def cached[Req: CachableKey, Res: CachableResult](timeToStale: Duration, timeToDead: Duration, maxSize: Int)( delegate: ServiceTag[T, Req, Res]): ServiceTag[T, Req, Res]
+  def cached[Req: CachableKey, Res: CachableResult](timeToStale: Duration, timeToDead: Duration, maxSize: Int)(delegate: ServiceTag[T, Req, Res]): ServiceTag[T, Req, Res]
 
   def profiled[Req, Res](delegate: ServiceTag[T, Req, Res])(implicit timeService: NanoTimeService): ServiceTag[T, Req, Res]
 
@@ -22,27 +24,44 @@ trait IService[T] {
 
 
   def enrich[Req, Res, ResE, ReqC, ResC](parent: ServiceTag[T, Req, Res],
-                                         child: ServiceTag[T, ReqC, ResC],
-                                         childReqs: Res => Seq[ReqC],
-                                         enricher: (Res, Seq[ResC]) => ResE): ServiceTag[T, Req, ResE]
+                                         child: ServiceTag[T, ReqC, ResC])
+                                        (implicit
+                                         enricher: Enricher[ResE, Res, ResC],
+                                         children: HasChildren[Res, ReqC]): ServiceTag[T, Req, ResE]
 
   def merge[ReqF, ResF, Req1, Res1, Req2, Res2](first: ServiceTag[T, Req1, Res1],
                                                 second: ServiceTag[T, Req2, Res2],
-                                                firstReq: ReqF => Req1,
-                                                secondReq: ReqF => Req2,
-                                                merger: (Res1, Res2) => ResF): ServiceTag[T, ReqF, ResF]
+                                                merger: (Res1, Res2) => ResF)
+                                               (implicit reqMtoReq1: ReqF => Req1, reqMtoReq2: ReqF => Req2): ServiceTag[T, ReqF, ResF]
+
+  def aggregate[P, C](serviceTagP: P, serviceTagC: C) = (serviceTagP, serviceTagC)
+
+  implicit class AggregatePimper[Req1, Res1, Req2, Res2](tuple: (ServiceTag[T, Req1, Res1], ServiceTag[T, Req2, Res2])) {
+    def enrich[ResE](implicit enricher: Enricher[ResE, Res1, Res2], children: HasChildren[Res1, Req2]): ServiceTag[T, Req1, ResE] =
+      IService.this.enrich[Req1, Res1, ResE, Req2, Res2](tuple._1, tuple._2)
+
+    def merge[ReqE, ResE](merger: (Res1, Res2) => ResE)(implicit reqMtoReq1: ReqE => Req1, reqMtoReq2: ReqE => Req2): ServiceTag[T, ReqE, ResE] =
+      IService.this.merge(tuple._1, tuple._2, merger)
+  }
+
 }
 
 
 trait IHttpSetup[T, HttpReq, HttpRes] extends IService[T] {
   def rawService(name: String): ServiceTag[T, HttpReq, HttpRes]
+
+  def getCachedProfiledObject[Req: ClassTag : ToServiceRequest : CachableKey, Res: ParserFinder : ClassTag : CachableResult]
+  (timeToStale: Duration, timeToDead: Duration, maxSize: Int, rawService: ServiceTag[T, HttpReq, HttpRes])
+  (implicit toHttpReq: (ServiceRequest) => HttpReq, nanoTimeService: NanoTimeService, toHttpResponse: ToServiceResponse[HttpRes]): ServiceTag[T, Req, Res] = {
+    cached[Req, Res](timeToStale, timeToDead, maxSize)(profiled[Req, Res](httpCallout[Req, Res, HttpReq, HttpRes](rawService)))
+  }
 }
 
 
 object ServiceInterpreters {
 
   class ServiceToString[HttpReq, HttpRes] extends IHttpSetup[String, HttpReq, HttpRes] {
-    override def cached[Req: CachableKey, Res: CachableResult](timeToStale: Duration, timeToDead: Duration, maxSize: Int)( delegate: ServiceTag[String, Req, Res]): ServiceTag[String, Req, Res] =
+    override def cached[Req: CachableKey, Res: CachableResult](timeToStale: Duration, timeToDead: Duration, maxSize: Int)(delegate: ServiceTag[String, Req, Res]): ServiceTag[String, Req, Res] =
       ServiceTag(s"Cached(${timeToStale}, ${timeToDead}, $maxSize) ~~~> ${delegate.t}")
 
     override def httpCallout[Req: ClassTag : ToServiceRequest, Res: ParserFinder : ClassTag, HttpReq, HttpRes](t: ServiceTag[String, HttpReq, HttpRes])
@@ -54,13 +73,13 @@ object ServiceInterpreters {
     override def profiled[Req, Res](delegate: ServiceTag[String, Req, Res])(implicit timeService: NanoTimeService): ServiceTag[String, Req, Res] =
       ServiceTag(s"Profile ~~> ${delegate.t}")
 
-    override def enrich[Req, Res, ResE, ReqC, ResC](parent: ServiceTag[String, Req, Res], child: ServiceTag[String, ReqC, ResC], childReqs: (Res) => Seq[ReqC], enricher: (Res, Seq[ResC]) => ResE): ServiceTag[String, Req, ResE] =
+    override def rawService(name: String): ServiceTag[String, HttpReq, HttpRes] = ServiceTag(s"RawService($name)")
+
+    override def enrich[Req, Res, ResE, ReqC, ResC](parent: ServiceTag[String, Req, Res], child: ServiceTag[String, ReqC, ResC])(implicit enricher: Enricher[ResE, Res, ResC], children: HasChildren[Res, ReqC]): ServiceTag[String, Req, ResE] =
       ServiceTag(s"Enrich(${parent.t}), ${child.t})")
 
-    override def merge[ReqF, ResF, Req1, Res1, Req2, Res2](first: ServiceTag[String, Req1, Res1], second: ServiceTag[String, Req2, Res2], firstReq: (ReqF) => Req1, secondReq: (ReqF) => Req2, merger: (Res1, Res2) => ResF) =
+    override def merge[ReqF, ResF, Req1, Res1, Req2, Res2](first: ServiceTag[String, Req1, Res1], second: ServiceTag[String, Req2, Res2], merger: (Res1, Res2) => ResF)(implicit reqMtoReq1: (ReqF) => Req1, reqMtoReq2: (ReqF) => Req2): ServiceTag[String, ReqF, ResF] =
       ServiceTag(s"Merge(${first.t}, ${second.t}")
-
-    override def rawService(name: String): ServiceTag[String, HttpReq, HttpRes] = ServiceTag(s"RawService($name)")
   }
 
 }
