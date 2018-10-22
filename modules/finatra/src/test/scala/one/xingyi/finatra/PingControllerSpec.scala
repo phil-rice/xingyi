29b9ab0f02ce1d58b851782c84b032fa9b64b6fb
Phil <phil.rice@iee.org>
2018-10-22 07:48:37 +0200

    Ping ControllerSpec

---
+++ modules/finatra/src/test/scala/one/xingyi/finatra/PingControllerSpec.scala
@@ -0,0 +1,20 @@
+package one.xingyi.finatra
+
+import com.twitter.finagle.http.Status
+import com.twitter.finatra.http.EmbeddedHttpServer
+import com.twitter.inject.server.FeatureTest
+import one.xingyi.core.UtilsSpec
+
+class PingControllerSpec extends FeatureTest {
+
+  override val server = new EmbeddedHttpServer(new FinatraServer(9004, new PingController))
+
+
+  test("ExampleServer#perform feature") {
+    server.httpGet(
+      path = "/ping",
+      andExpect = Status.Ok,
+      withBody = "pong")
+  }
+
+}
