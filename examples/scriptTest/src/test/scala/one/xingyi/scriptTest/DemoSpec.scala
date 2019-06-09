447cfc5baad13eb9c91e5c04871bcfab19039e27
Phil <phil.rice@iee.org>
2019-06-09 15:17:02 +0200

    Added travis bypass and better selenium tests

--- examples/scriptTest/src/test/scala/one/xingyi/scriptTest/DemoSpec.scala
+++ examples/scriptTest/src/test/scala/one/xingyi/scriptTest/DemoSpec.scala
@@ -1,23 +1,33 @@
 package one.xingyi.scriptTest
-import com.sun.net.httpserver.HttpServer
 import one.xingyi.core.UtilsSpec
-import one.xingyi.core.http.{Get, ServiceRequest, Uri}
 import one.xingyi.core.simpleServer.SimpleHttpServer
-import one.xingyi.scriptBackend1.{Backend1, Person}
+import one.xingyi.scriptBackend1.Backend1
 import one.xingyi.scriptBackend2.Backend2
 import one.xingyi.scriptBackend3.Backend3
-import one.xingyi.scriptWebsite.{Website, WebsiteApp}
+import one.xingyi.scriptWebsite.WebsiteApp
+import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
 import org.scalatest.concurrent.Eventually
-import org.scalatest.selenium.HtmlUnit
+import org.scalatest.selenium.{Chrome, HtmlUnit, WebBrowser}
 import org.scalatest.time.{Millis, Span}
-import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, FlatSpec, Matchers}
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
-abstract class DemoSpec[P] extends FlatSpec with Matchers with BeforeAndAfter with Eventually with UtilsSpec with HtmlUnit {
+abstract class DemoSpec[P] extends FlatSpec with Matchers with BeforeAndAfter with Eventually with UtilsSpec with WebBrowser {
 
-
-  val host = "http://localhost:9000/"
+  //  webDriver.setJavascriptEnabled(true)
+  val host = "http://localhost:9000"
+  implicit val webDriver = {
+    if (System.getenv("travis") == null) {
+      import org.openqa.selenium.chrome.ChromeOptions
+      val chromeDriverPath = "./Chromedriver.exe"
+      System.setProperty("webdriver.chrome.driver", chromeDriverPath)
+      val options = new ChromeOptions
+      options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200", "--ignore-certificate-errors")
+      new ChromeDriver(options);
+    } else null
+  }
 
   override implicit val patienceConfig = PatienceConfig(timeout = scaled(Span(10000, Millis)), interval = scaled(Span(1000, Millis)))
+
   val websiteApp = new WebsiteApp
   var websiteServer: SimpleHttpServer = null
 
@@ -26,49 +36,56 @@ abstract class DemoSpec[P] extends FlatSpec with Matchers with BeforeAndAfter wi
   def startBackend()
   def stopBackend()
 
-  def pToNameLine1Line2(any: Any): (String, String, String) = ???
 
   behavior of s"Website with backend $backendName"
 
   it should "test we can actually read a person from the backend" in {
-    println("in test")
-//    goTo(host + "/person/someName/edit")
-//    pageTitle shouldBe "Xing Yi Demo"
-//    textField("name").value shouldBe "someName"
-//    textField("line1").value shouldBe "line1"
-//    textField("line2").value shouldBe "line2"
-//
-//    textField("line1").value = "newLine1"
-//    textField("line2").value = "newLine2"
-//    submit
-//    goTo(host + "/person/someName/edit")
-//    eventually {
-//      textField("name").value shouldBe "someName"
-//      textField("line1").value shouldBe "line1"
-//      textField("line2").value shouldBe "line2"
-//
-//    }
+    if (webDriver != null) {
+      println("in test")
+      goTo(host + "/person/someName/edit")
+      //    println("page source")
+      //    println(pageSource)
+      pageTitle shouldBe "Xing Yi demo"
+      textField("name").value shouldBe "someName"
+      //    textField("line1").value shouldBe "line1"
+      //    textField("line2").value shouldBe "line2"
+
+      textField("line1").value = "newLine1"
+      textField("line2").value = "newLine2"
+      submit
+      //    Thread.sleep(2000)
+      goTo(host + "/person/someName/edit")
+      eventually {
+        textField("name").value shouldBe "someName"
+        textField("line1").value shouldBe "newLine1"
+        textField("line2").value shouldBe "newLine2"
+
+      }
+    }
   }
 
 
   before {
-    eventually { // there are multiple backends
-      // /websites that need to bind, so wihtout this we get non deterministic failures
-      println(s"Attempting to start $backendName website")
-      websiteServer = websiteApp.cheapServer.start
-      println(s"Attempting to start $backendName backed")
-      startBackend()
-    }
-    println(s"Succeeded in starting both for $backendName")
+    if (webDriver != null)
+      eventually { // there are multiple backends
+        // /websites that need to bind, so wihtout this we get non deterministic failures
+        println(s"Attempting to start $backendName website")
+        websiteServer = websiteApp.cheapServer.start
+        println(s"Attempting to start $backendName backed")
+        startBackend()
+        println(s"Succeeded in starting both for $backendName")
+      }
   }
 
 
   after {
-    println(s"starting to close websiteApp for $backendName")
-    websiteApp.cheapServer.stop(websiteServer)
-    println(s"starting to close backend for $backendName")
-    stopBackend()
-    println(s"closed everything $backendName")
+    if (webDriver != null) {
+      println(s"starting to close websiteApp for $backendName")
+      websiteApp.cheapServer.stop(websiteServer)
+      println(s"starting to close backend for $backendName")
+      stopBackend()
+      println(s"closed everything $backendName")
+    }
   }
 }
 
